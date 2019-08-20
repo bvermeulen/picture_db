4a4517a7f4b2a6bb7ade99b6c77dae96a428aed8
bvermeulen <bruno_vermeulen2001@yahoo.com>
2019-08-20 12:26:56 +0700

    initial commit picture_database

--- picture_db.py
+++ picture_db.py
@@ -10,8 +10,11 @@ import piexif
 
 
 class Exif:
+    ''' utility methods to handle picture exif
+    '''
     codec = 'ISO-8859-1'  # or latin-1
 
+    @classmethod
     def exif_to_tag(cls, exif_dict):
         exif_tag_dict = {}
         thumbnail = exif_dict.pop('thumbnail')
@@ -30,11 +33,89 @@ class Exif:
 
         return exif_tag_dict
 
+    @staticmethod
+    def convert_gps(gps_latitude, gps_longitude, gps_altitude):
+        ''' input based on tuples of fractions
+        '''
+        def convert_to_degrees(lat_long_value):
+            ref = lat_long_value.get('ref', '')
+            fractions = lat_long_value.get('pos', [0, 1])
+            degrees = fractions[0][0] / fractions[0][1]
+            minutes = fractions[1][0] / fractions[1][1]
+            seconds = fractions[2][0] / fractions[2][1]
+
+            if fractions[1][0] == 0 and fractions[2][0] == 0:
+                return f'{ref} {degrees:.4f}\u00B0'
+
+            elif fractions[2][0] == 0:
+                return f'{ref} {degrees:.0f}\u00B0 {minutes:.2f}"'
+
+            else:
+                return f'{ref} {degrees:.0f}\u00B0 {minutes:.0f}" {seconds:.0f}\''
+
+        try:
+            latitude = convert_to_degrees(gps_latitude)
+            longitude = convert_to_degrees(gps_longitude)
+            alt_fraction = gps_altitude.get('alt')
+            altitude = f'{alt_fraction[0]/ alt_fraction[1]:.2f}'
+
+            return latitude, longitude, altitude
+
+        except (TypeError, AttributeError, ZeroDivisionError) as error:
+            print(f'unable to convert coordinate: {error}')
+            return None, None, None
+
+    @classmethod
     def exif_to_json(cls, exif_tag_dict):
         try:
             return json.dumps(exif_tag_dict)
         except Exception as e:
             print(f'Convert exif to tag first, error is {e}')
+            raise()
+
+
+class DbUtils:
+    '''  utility methods for database
+    '''
+    host = 'localhost'
+    db_user = 'db_tester'
+    db_user_pw = 'db_tester_pw'
+    database = 'picture_base'
+
+    @classmethod
+    def connect(cls, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            connect_string = f'host=\'{cls.host}\' dbname=\'{cls.database}\''\
+                             f'user=\'{cls.db_user}\' password=\'{cls.db_user_pw}\''
+            result = None
+            try:
+                connection = psycopg2.connect(connect_string)
+                cursor = connection.cursor()
+                result = func(*args, cursor, **kwargs)
+                connection.commit()
+
+            except psycopg2.Error as error:
+                print(f'error while connect to PostgreSQL {cls.database}: '
+                      f'{error}')
+
+            finally:
+                if connection:
+                    cursor.close()
+                    connection.close()
+                    print(f'PostgreSQL connection to {cls.database} is closed')
+
+            return result
+
+        return wrapper
+
+    @staticmethod
+    def get_cursor(cursor):
+        if cursor:
+            return cursor[0]
+
+        else:
+            print('unable to connect to database')
             raise()
 
 
@@ -47,48 +128,11 @@ class PictureDb:
                                'gps_latitude, gps_longitude, gps_altitude,'
                                'gps_img_direction, thumbnail, exif')
 
-
-    class DbDecorators:
-        host = 'localhost'
-        db_user = 'db_tester'
-        db_user_pw = 'db_tester_pw'
-        database = 'picture_base'
-
-        @classmethod
-        def connect(cls, func):
-            @wraps(func)
-            def wrapper(*args, **kwargs):
-                connect_string = f'host=\'{cls.host}\' dbname=\'{cls.database}\''\
-                                f'user=\'{cls.db_user}\' password=\'{cls.db_user_pw}\''
-                result = None
-                try:
-                    with psycopg2.connect(connect_string) as connection:
-                        cursor = connection.cursor()
-                        result = func(*args, cursor, **kwargs)
-
-                except psycopg2.Error as error:
-                    print(f'error while connect to PostgreSQL {cls.database}: '
-                          f'{error}')
-
-                finally:
-                    if connection:
-                        cursor.close()
-                        connection.close()
-                        print(f'PostgreSQL connection to {cls.database} is closed')
-                return result
-            return wrapper
-
-
-    @DbDecorators.connect
+    @classmethod
+    @DbUtils.connect
     def create_pictures_table(cls, *args):
-        if args:
-            cursor = args[0]
+        cursor = DbUtils().get_cursor(args)
 
-        else:
-            print(f'no connection to database')
-            return
-
-        # create table
         pics_tbl = cls.PicturesTable(
             id='id SERIAL PRIMARY KEY',
             file_path='file_path VARCHAR(250)',
@@ -96,7 +140,7 @@ class PictureDb:
             file_last_modified='file_modified TIMESTAMP',
             file_created='file_created TIMESTAMP',
             file_size='file_size INTEGER',
-            date_picture='pic_date TIMESTAMP',
+            date_picture='date_picture TIMESTAMP',
             camera_make='camera_make VARCHAR(20)',
             camera_model='camera_model VARCHAR(20)',
             gps_latitude='gps_latitude JSON',
@@ -118,14 +162,10 @@ class PictureDb:
         print(sql_string)
         cursor.execute(sql_string)
 
-    @DbDecorators.connect
+    @classmethod
+    @DbUtils.connect
     def store_picture_meta(cls, filename, *args):
-        if args:
-            cursor = args[0]
-
-        else:
-            print(f'no connection to database')
-            return
+        cursor = DbUtils().get_cursor(args)
 
         # file attributes
         file_stat = os.stat(filename)
@@ -179,7 +219,6 @@ class PictureDb:
         if not thumbnail:
             size = (180, 180)
             im.thumbnail(size, Image.ANTIALIAS)
-            im.show()
             img_bytes = io.BytesIO()
             im.save(img_bytes, format='JPEG')
             thumbnail = json.dumps(img_bytes.getvalue().decode(Exif().codec))
@@ -188,7 +227,7 @@ class PictureDb:
 
         sql_string = f'INSERT INTO {cls.table_name} ('\
                      f'file_path, file_name, file_modified, file_created, file_size, '\
-                     f'pic_date, camera_make, camera_model, '\
+                     f'date_picture, camera_make, camera_model, '\
                      f'gps_latitude, gps_longitude, gps_altitude, gps_img_dir, '\
                      f'thumbnail, exif) '\
                      f'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'
@@ -200,14 +239,10 @@ class PictureDb:
                                     gps_altitude, gps_img_direction, thumbnail,
                                     exif_json))
 
-    @DbDecorators.connect
+    @classmethod
+    @DbUtils.connect
     def load_picture_meta(cls, _id, *args):
-        if args:
-            cursor = args[0]
-
-        else:
-            print(f'no connection to database')
-            return
+        cursor = DbUtils().get_cursor(args)
 
         sql_string = f'SELECT * FROM {cls.table_name} where id = \'{_id}\';'
         print(sql_string)
@@ -242,6 +277,11 @@ class PictureDb:
             if key != 'thumbnail':
                 print(f'{key}: {val}')
 
+        latitude, longitude, altitude = Exif().\
+            convert_gps(pic_meta.gps_latitude, pic_meta.gps_longitude,
+                        pic_meta.gps_altitude)
+        print(f'coordinate: {latitude}, {longitude}, altitude: {altitude}')
+
 
 def test():
     filepath = './pics/'
@@ -251,11 +291,11 @@ def test():
                  'moonwalk.jpg', 'Various 013.jpg', 'Various 018.jpg']
 
     picdb = PictureDb()
-    # picdb.create_pictures_table()
+    picdb.create_pictures_table()
 
-    # for pic in filenames:
-    #     filename = filepath + pic
-    #     picdb.store_picture_meta(filename)
+    for pic in filenames:
+        filename = filepath + pic
+        picdb.store_picture_meta(filename)
 
     picdb.load_picture_meta(10)
 
